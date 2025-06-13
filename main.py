5c059c70bf2917982f6a8c17e45c5a36a3f4cda9
Hugo <hugo.bernaltorales2@gmail.com>
2025-06-12 22:44:46 -0300

    agregando-codigo

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-a = 'Hello World'
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 
-print(a)
+import crud
+from database import engine, localSession
+from schemas import UserData, UserId
+from models import Base
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = localSession()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get('/')
+def root():
+    return 'Hi estoy probando mi api en mi pc personal'
