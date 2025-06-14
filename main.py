8c5ff60f1e0c81dc1b7ad6123e6985f52903d854
Hugo <hugo.bernaltorales2@gmail.com>
2025-06-13 23:54:41 -0300

    Agregado de docker con contenedor mysql para la base de datos

--- main.py
+++ main.py
@@ -24,5 +24,48 @@ def root():
 
 
 @app.get('/api/users', response_model=list[UserId])
-def get_users(db: Session = Depends(get_db())):
+def get_users(db: Session = Depends(get_db)):
     return crud.get_users(db=db)
+
+
+@app.get('/api/users/{id:int}', response_model=UserId)
+def get_user(id, db: Session = Depends(get_db)):
+    user_by_id = crud.get_user_by_id(db=db, id=id)
+    if user_by_id:
+        return user_by_id
+    raise HTTPException(status_code=404, detail='User not found')
+
+
+@app.post('/api/users/', response_model=UserId)
+def create_user(user: UserData, db: Session = Depends(get_db)):
+    check_name = crud.get_user_by_name(db=db, name=user.name)
+    if check_name:
+        raise HTTPException(status_code=400, detail='User already exist')
+    return crud.create_user(db=db, user=user)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
